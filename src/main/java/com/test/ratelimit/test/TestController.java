47dbdc2f99a51dbf5b4089a9b8b6880d042761ca
Byuk_mm <64072741+JeongSangByuk@users.noreply.github.com>
2024-07-26 04:32:11 +0900

    Update TestController.java

--- src/main/java/com/test/ratelimit/test/TestController.java
+++ src/main/java/com/test/ratelimit/test/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @GetMapping("/test")
     public String test() {
 
-        return "testttt : " + instanceId;
+        return "final testttt : " + instanceId;
     }
 
 
